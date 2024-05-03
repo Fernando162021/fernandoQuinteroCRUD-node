d7c79a672d8efcbd69d4f41d2706ad519053f5ef
Fernando162021 <fernandoquinteromps@outlook.com>
2024-05-02 20:32:07 -0600

    final

--- routes/libros-routes.js
+++ routes/libros-routes.js
@@ -5,7 +5,7 @@ const librosController = require("../controllers/libros-controller")
 router.post("/", librosController.createLibro)
 router.put("/:id", librosController.updateLibro)
 router.get("/", librosController.findAllLibros)
-router.get("/:id", librosController.findLibrobyID)
+router.get("/:isbn", librosController.findLibrobyISBN)
 router.delete("/:id", librosController.deleteLibro)
 
 module.exports = router
